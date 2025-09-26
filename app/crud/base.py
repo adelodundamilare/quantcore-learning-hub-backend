2d7acd3a041ba37dce78ceb7e18853b7d139e445
Adelodun Damilare <adelodundamilare@yahoo.com>
2025-09-26 10:58:21 +0100

    fix update

--- app/crud/base.py
+++ app/crud/base.py
@@ -60,22 +60,22 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         db_obj: ModelType,
         obj_in: Union[UpdateSchemaType, Dict[str, Any]] | None = None
     ) -> ModelType:
-        if obj_in is None:
-            # Assume db_obj was modified directly
-            db.add(db_obj)
-        else:
-            obj_data = jsonable_encoder(db_obj)
-            if isinstance(obj_in, dict):
-                update_data = obj_in
-            else:
-                update_data = obj_in.model_dump(exclude_unset=True)
-            for field in obj_data:
-                if field in update_data:
-                    setattr(db_obj, field, update_data[field])
-            db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
+        try:
+            if db_obj not in db:
+                db_obj = db.merge(db_obj)
+
+            if obj_in is not None:
+                update_data = obj_in if isinstance(obj_in, dict) else obj_in.model_dump(exclude_unset=True)
+                for field, value in update_data.items():
+                    if hasattr(db_obj, field):
+                        setattr(db_obj, field, value)
+
+            db.commit()
+            db.refresh(db_obj)
+            return db_obj
+        except Exception:
+            db.rollback()
+            raise
 
     def delete(self, db: Session, *, id: int) -> ModelType:
         obj = db.query(self.model).get(id)
