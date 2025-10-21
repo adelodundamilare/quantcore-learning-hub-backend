53df0fa1a40d80c8edec7e63c2af4515b6900118
Adelodun Damilare <adelodundamilare@yahoo.com>
2025-10-21 19:12:31 +0100

    fix trading detail to student profile fetch

--- app/crud/transaction.py
+++ app/crud/transaction.py
@@ -1,8 +1,17 @@
+from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
 from app.models.transaction import Transaction
 from app.schemas.transaction import TransactionCreate, TransactionSchema
+from typing import List
 
 class CRUDTransaction(CRUDBase[Transaction, TransactionCreate, TransactionSchema]):
-    pass
+    def get_multi_by_user_and_type(
+        self, db: Session, *, user_id: int, transaction_type: str
+    ) -> List[Transaction]:
+        return (
+            db.query(self.model)
+            .filter(self.model.user_id == user_id, self.model.transaction_type == transaction_type)
+            .all()
+        )
 
 transaction = CRUDTransaction(Transaction)
