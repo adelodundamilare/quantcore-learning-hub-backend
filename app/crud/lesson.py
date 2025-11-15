9046a76eb11b39a8bc02b19f7ef941679271928d
Adelodun Damilare <adelodundamilare@yahoo.com>
2025-11-15 09:31:37 +0100

    fix curriculum soft delete

--- app/crud/lesson.py
+++ app/crud/lesson.py
@@ -7,6 +7,6 @@ from app.schemas.lesson import LessonCreate, LessonUpdate
 
 class CRUDLesson(CRUDBase[Lesson, LessonCreate, LessonUpdate]):
     def get_by_curriculum(self, db: Session, *, curriculum_id: int) -> List[Lesson]:
-        return db.query(self.model).filter(self.model.curriculum_id == curriculum_id).order_by(self.model.order).all()
+        return db.query(self.model).filter(self.model.curriculum_id == curriculum_id, self.model.deleted_at == None).order_by(self.model.order).all()
 
 lesson = CRUDLesson(Lesson)
